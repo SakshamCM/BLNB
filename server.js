13716eeb7e703b8f396e3307766e88d3f338e581
SakshamVerma2004 <reemaverma001@gmail.com>
2024-09-15 13:07:38 +0530

    restricting image size to 10 mb

--- server.js
+++ server.js
@@ -78,13 +78,13 @@ let port = 4000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.json({ limit: "5000mb" }));
-app.use(express.urlencoded({ limit: "5000mb", extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 let storage = multer.memoryStorage();
 let upload = multer({
   storage: storage,
-  limits: { fileSize: 5000 * 1024 * 1024 },
+  limits: { fileSize: 10 * 1024 * 1024 },
 });
 
 app.get("/", (request, response) => {
