4c77cb639f6a69fcdbb9ad038ad2a412cf5c2cfb
Creative-Monk-Team <team.creativemonk@gmail.com>
2024-10-15 17:18:58 +0530

    increased the limit of the data

--- server.js
+++ server.js
@@ -212,13 +212,13 @@ let port = 4000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.json({ limit: "30mb" }));
-app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 let storage = multer.memoryStorage();
 let upload = multer({
   storage: storage,
-  limits: { fileSize: 30 * 1024 * 1024 },
+  limits: { fileSize: 50 * 1024 * 1024 },
 });
 
 app.get("/", (request, response) => {
